24ab73e0dca8234fcef251debd12ac9f66bb3cf9
Jan-Willem Gmelig Meyling <jan-willem@youngmediaexperts.nl>
2014-09-09 20:24:19 +0200

    Some more changes after merge

--- src/main/java/nl/tudelft/ti2206/game/Cannon.java
+++ src/main/java/nl/tudelft/ti2206/game/Cannon.java
@@ -1,14 +1,17 @@
 package nl.tudelft.ti2206.game;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
-import nl.tudelft.ti2206.throwaway.Bubble;
-import nl.tudelft.ti2206.throwaway.GuiThrowAway;
+import nl.tudelft.ti2206.bubbles.Bubble;
+import nl.tudelft.ti2206.bubbles.Sprite;
 
-public class Cannon {
-	public final int x, y;
+public class Cannon implements Sprite {
+	public int x, y;
 	public double angle = 0;
 	public Bubble nextBubble, loadedBubble;
 	public final boolean mouseControl;
@@ -25,19 +28,49 @@ public class Cannon {
 		this.mouseControl = mouseControl;
 	}
 	
-	public void update() {
-		if (mouseControl) {
-			Point mousePosition = GuiThrowAway.instance.getMousePosition();
-			if (mousePosition != null) {
-				angle = Math.atan2(y - mousePosition.y, mousePosition.x - x);
+	public void bindMouseListenerTo(final Component component) {
+		component.addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
 			}
-		}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				angle = Math.atan2(y - e.getY(), e.getX() - x);
+				component.repaint();
+			}
+			
+		});
 	}
 	
-	public void render(Graphics g) {
+	public void render(final Graphics g) {
 		g.setColor(Color.blue);
 		g.drawRect(x - 8, y - 8, 16, 16);
 		g.setColor(Color.green);
 		g.drawLine(x, y, (int) (x + Math.cos(angle) * 50), (int) (y - Math.sin(angle) * 50));
+	}
+
+	@Override
+	public void setPosition(final Point position) {
+		this.x = position.x;
+		this.y = position.y;
+	}
+
+	@Override
+	public Point getPosition() {
+		return new Point(x,y);
+	}
+
+	@Override
+	public int getX() {
+		return x;
+	}
+
+	@Override
+	public int getY() {
+		return y;
 	}
 }
--- src/main/java/nl/tudelft/ti2206/throwaway/GuiThrowAway.java
+++ src/main/java/nl/tudelft/ti2206/throwaway/GuiThrowAway.java
@@ -1,27 +1,51 @@
 package nl.tudelft.ti2206.throwaway;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import javax.swing.JFrame;
 
 import nl.tudelft.ti2206.game.MouseListener;
 
 public class GuiThrowAway extends JFrame {
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3473878919673841174L;
+	
 	public static GuiThrowAway instance;
-	public boolean gameEnded = false;
-	public GuiThrowAwayPanel GUI;
+	
+	private final GuiThrowAwayPanel GUI;
+	private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1); 
 	
 	public GuiThrowAway() {
 		super("Bubble Shooter");
-		GuiThrowAwayPanel GUI = new GuiThrowAwayPanel();
+		GUI = new GuiThrowAwayPanel();
+		
 		this.add(GUI);
 		this.pack();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
 		this.addMouseListener(MouseListener.INSTANCE);
 		this.setLocationRelativeTo(null);
+		
+		run();
+	}
+	
+	public long time = System.currentTimeMillis();
+
+	private void run() {
+		executorService.scheduleAtFixedRate(new Runnable() {
+
+			@Override
+			public void run() {
+				time = System.currentTimeMillis();
+			}
+			
+		}, 0, 33, TimeUnit.MILLISECONDS);
 	}
 	
 }
