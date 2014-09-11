6cd639fecadcd340a33d0f54ecb2b21aa3034fd6
Leon <leonhoek2@gmail.com>
2014-09-11 09:55:29 +0200

    GUI for multiplayer, buttons not yet working

--- src/main/java/nl/tudelft/ti2206/game/GUI.java
+++ src/main/java/nl/tudelft/ti2206/game/GUI.java
@@ -2,7 +2,10 @@ package nl.tudelft.ti2206.game;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import nl.tudelft.ti2206.throwaway.GuiThrowAwayPanel;
 
@@ -10,17 +13,37 @@ import java.awt.ComponentOrientation;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class GUI {
 
-	private JFrame frame;
+	JFrame frame;
+	GuiThrowAwayPanel gamePanel;
 	
+	//multiplayer on same machine
+	GuiThrowAwayPanel player2Panel;
+	final boolean multiplayer = true;
+	
+	//Score-labels
+	JLabel playerScore;
+	JLabel player2Score;
+	
+	//game-variables
+	boolean game_is_running = true;
+	public long time = System.currentTimeMillis();
+	//private static final Logger log = LoggerFactory.getLogger(GUI.class);
+	private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+	
+	//gridbag constants
 	final static boolean shouldFill = true;
 	final static boolean shouldWeightX = true;
 	final static boolean RIGHT_TO_LEFT = false;
 	
 	
-	public void fillGameFrame(Container pane){
+	protected void fillGameFrame(Container pane){
 		if(RIGHT_TO_LEFT){
 			pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
 		}
@@ -28,13 +51,13 @@ public class GUI {
 		GridBagConstraints c = new GridBagConstraints();
 		
 		// everything the frame must be filled with
-		JPanel gamePanel = new GuiThrowAwayPanel();
+		gamePanel = new GuiThrowAwayPanel();
 		gamePanel.setMinimumSize(gamePanel.getPreferredSize());
 		c.fill = GridBagConstraints.NONE;
 		c.weightx = 0;
 		c.weighty = 0;
 		c.gridheight = 4;
-		c.gridwidth = GridBagConstraints.RELATIVE;
+		c.gridwidth = 1;
 		c.anchor = GridBagConstraints.EAST;
 		c.gridx = 0;
 		c.gridy = 0;
@@ -80,6 +103,24 @@ public class GUI {
 		pane.add(menu, c);
 		//JLabel 
 		
+		//multiplayer
+		// everything the frame must be filled with
+		if(multiplayer){
+			player2Panel = new GuiThrowAwayPanel();
+			player2Panel.setMinimumSize(player2Panel.getPreferredSize());
+			player2Panel.setMaximumSize(player2Panel.getPreferredSize());
+			c.fill = GridBagConstraints.NONE;
+			c.weightx = 0;
+			c.weighty = 0;
+			c.gridheight = 4;
+			c.gridwidth = 1;
+			c.anchor = GridBagConstraints.WEST;
+			c.gridx = 3;
+			c.gridy = 0;
+			pane.add(player2Panel,c);
+			
+		}
+		
 	}
 
 	public GUI(){
@@ -93,7 +134,29 @@ public class GUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		
+		while(game_is_running){
+			run();
+			
+		}
+	}
+	
+	protected void update(){
+		gamePanel.gameStep();
+		if(multiplayer){
+			 player2Panel.gameStep();
+		}
 		
-		
+	}
+	private void run() {
+		executorService.scheduleAtFixedRate(new Runnable() {
+
+			@Override
+			public void run() {
+				time = System.currentTimeMillis();
+				//log.info("Game step at {}", time);
+				update();
+			}
+			
+		}, 0, 33, TimeUnit.MILLISECONDS);
 	}
 }
