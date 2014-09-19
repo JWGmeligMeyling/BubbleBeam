fe37ea5f616cdf23e5b66fdc21b26aebbc0ed9f7
Sam Smulders <sam_smulders@live.nl>
2014-09-19 16:47:47 +0200

    Added use of generics in PacketHandler & PacketListener

--- src/main/java/nl/tudelft/ti2206/network/packets/PacketListener.java
+++ src/main/java/nl/tudelft/ti2206/network/packets/PacketListener.java
@@ -6,21 +6,6 @@ package nl.tudelft.ti2206.network.packets;
  * 
  * @author Sam Smulders
  */
-public interface PacketListener {
-	
-	public interface CannonRotate extends PacketListener {
-		void update(Packet.CannonRotate packet);
-	}
-	
-	public interface CannonShoot extends PacketListener {
-		void update(Packet.CannonShoot packet);
-	}
-	
-	public interface BubbleMeshSync extends PacketListener {
-		void update(Packet.BubbleMeshSync packet);
-	}
-	
-	public interface LoadNewBubble extends PacketListener {
-		void update(Packet.LoadNewBubble packet);
-	}
+public interface PacketListener<T extends Packet> {
+	public void update(T packet);
 }
