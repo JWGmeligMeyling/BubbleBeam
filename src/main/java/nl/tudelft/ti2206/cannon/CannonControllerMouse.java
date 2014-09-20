3086715debdf8e0567679a854e6f41d8e9b7d381
Sam Smulders <sam_smulders@live.nl>
2014-09-20 14:01:42 +0200

    Some more work on the Cannon & Room

--- src/main/java/nl/tudelft/ti2206/cannon/CannonControllerMouse.java
+++ src/main/java/nl/tudelft/ti2206/cannon/CannonControllerMouse.java
@@ -4,7 +4,19 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class CannonControllerMouse implements MouseMotionListener, MouseListener {
+import nl.tudelft.util.Vector2f;
+
+public class CannonControllerMouse implements CannonController, MouseMotionListener, MouseListener {
+	
+	protected Cannon2 cannon;
+	private Vector2f direction;
+	private Room room;
+	
+	@Override
+	public void setCannon(Cannon2 cannon) {
+		this.cannon = cannon;
+		this.room = cannon.room;
+	}
 	
 	@Override
 	public void mouseDragged(MouseEvent e) {
@@ -12,6 +24,15 @@ public class CannonControllerMouse implements MouseMotionListener, MouseListener
 	
 	@Override
 	public void mouseMoved(MouseEvent e) {
+		Vector2f dir = (new Vector2f(e.getPoint()).subtract(new Vector2f(cannon.position)))
+				.normalize();
+		dir.y = Math.abs(direction.y);
+		if (dir.y < Cannon2.MIN_DIRECTION_Y) {
+			dir.x = Cannon2.MIN_DIRECTION_X * Math.signum(direction.x);
+			dir.y = Cannon2.MIN_DIRECTION_Y;
+		}
+		cannon.angle = Math.atan(dir.y / dir.x);
+		direction = dir;
 	}
 	
 	@Override
@@ -20,6 +41,11 @@ public class CannonControllerMouse implements MouseMotionListener, MouseListener
 	
 	@Override
 	public void mousePressed(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1) {
+			if (room.canShoot()) {
+				room.shootBubble(direction);
+			}
+		}
 	}
 	
 	@Override
