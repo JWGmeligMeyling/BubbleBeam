692b1ceec3a013c81eadd8e8526f0043472daa96
Jan-Willem Gmelig Meyling <j.gmeligmeyling@student.tudelft.nl>
2014-09-28 13:41:42 +0200

    Small change for joker bubble

--- src/main/java/nl/tudelft/ti2206/bubbles/JokerBubble.java
+++ src/main/java/nl/tudelft/ti2206/bubbles/JokerBubble.java
@@ -7,6 +7,8 @@ public class JokerBubble implements DecoratedBubble {
 	private static final long serialVersionUID = -1415122920739845104L;
 	
 	protected final ColouredBubble bubble;
+	
+	protected boolean collided = false;
 
 	public JokerBubble() {
 		bubble = new ColouredBubble(Color.WHITE);
@@ -14,7 +16,7 @@ public class JokerBubble implements DecoratedBubble {
 	
 	@Override
 	public boolean popsWith(Bubble target) {
-		return true;
+		return !collided || bubble.popsWith(target);
 	}
 	
 	@Override
@@ -22,6 +24,7 @@ public class JokerBubble implements DecoratedBubble {
 		if(Coloured.class.isInstance(other)) {
 			Coloured coloured = Coloured.class.cast(other);
 			bubble.setColor(coloured.getColor());
+			collided = true;
 		}
 	}
 
