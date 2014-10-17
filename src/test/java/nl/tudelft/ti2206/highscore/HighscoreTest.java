c5e00308d2410339679333a40b3f4570824e2bea
Leon <leonhoek2@gmail.com>
2014-10-17 12:39:51 +0200

    fixed the failing test

--- src/test/java/nl/tudelft/ti2206/highscore/HighscoreTest.java
+++ src/test/java/nl/tudelft/ti2206/highscore/HighscoreTest.java
@@ -1,5 +1,7 @@
 package nl.tudelft.ti2206.highscore;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 public class HighscoreTest {
@@ -18,7 +20,7 @@ public class HighscoreTest {
 		ScoreItem test = new ScoreItem(1250,"Dwight");
 		highscore.addScore(test);
 		assert(highscore.getPlace(1).equals(item2));
-		ScoreItem holly = new ScoreItem(1270,"Holly");
+		ScoreItem holly = new ScoreItem(1270,"Holly",new Date(12));
 		highscore.addScore(holly);
 		assert(highscore.getPlace(3).equals(test));
 		assert(highscore.getPlace(4) == null);
