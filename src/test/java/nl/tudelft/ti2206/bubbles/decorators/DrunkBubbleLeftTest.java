0ed3ffb641c9f8f8d71b7651eb61b4c5641589be
Luka Bavdaz <lukabavdaz@gmail.com>
2014-10-18 20:13:33 +0200

    added drunkBubble tests

--- src/test/java/nl/tudelft/ti2206/bubbles/decorators/DrunkBubbleLeftTest.java
+++ src/test/java/nl/tudelft/ti2206/bubbles/decorators/DrunkBubbleLeftTest.java
@@ -3,18 +3,19 @@ package nl.tudelft.ti2206.bubbles.decorators;
 import static org.junit.Assert.*;
 import nl.tudelft.ti2206.bubbles.AbstractBubble;
 import nl.tudelft.ti2206.bubbles.AbstractBubbleTest;
+import nl.tudelft.util.Vector2f;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class DrunkBubbleLeftTest extends AbstractBubbleTest {
 
-	protected DrunkBubbleRight drunkBubble;
+	protected DrunkBubbleLeft drunkBubble;
 
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
-		bubble = drunkBubble = new DrunkBubbleRight(new AbstractBubble());
+		bubble = drunkBubble = new DrunkBubbleLeft(new AbstractBubble());
 	}
 	
 	@Override
@@ -23,7 +24,7 @@ public class DrunkBubbleLeftTest extends AbstractBubbleTest {
 	
 	@Test
 	public void testVelocityChange() {
-		
+		assertEquals(drunkBubble.velocityChange(),new Vector2f(-DrunkBubble.ACCELERATION,0f));
 	}
 
 }
