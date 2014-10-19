9e5397b13b9009acc7378e2710fc75b88aa78746
Leon <leonhoek2@gmail.com>
2014-10-19 16:41:19 +0200

    added buttons for gameModes

--- src/main/java/nl/tudelft/ti2206/game/actions/RestartSinglePlayerAction.java
+++ src/main/java/nl/tudelft/ti2206/game/actions/RestartSinglePlayerAction.java
@@ -1,15 +1,19 @@
 package nl.tudelft.ti2206.game.actions;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+
+import nl.tudelft.ti2206.game.SinglePlayerFrame;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import nl.tudelft.ti2206.game.SinglePlayerFrame;
 
 public class RestartSinglePlayerAction extends AbstractAction {
 
@@ -26,8 +30,102 @@ public class RestartSinglePlayerAction extends AbstractAction {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		
-		
+			final JDialog dialog = new JDialog(singlePlayerFrame, false);	//this makes the dialog always stay on top of the singleplayerframe
+			dialog.setTitle("Choose singleplayer gamemode");
+			
+			
+			
+			//adding buttons for all gamemodes to the dialog
+			dialog.setLayout(new GridLayout(1,4));
+			JButton classicButton = new JButton("Classic");
+			dialog.add(classicButton);
+			
+			JButton drunkButton = new JButton("Drunk mode");
+			dialog.add(drunkButton);
+			
+			JButton timedButton = new JButton("Timed mode");
+			dialog.add(timedButton);
+			
+			JButton powerupButton = new JButton("Power-up mode");
+			dialog.add(powerupButton);
+			
+			//create the universal actionlistener
+			ActionListener al = new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					dialog.dispose();
+					if(e.getSource().equals(timedButton)){
+						startTimed();
+					} else if(e.getSource().equals(drunkButton)){
+						startDrunk();
+					} else if(e.getSource().equals(classicButton)){
+						startClassic();
+					} else if(e.getSource().equals(powerupButton)){
+						startPowerup();
+					}
+				}
+			};
+			
+			//making the buttons functional
+			classicButton.addActionListener(al);
+			drunkButton.addActionListener(al);
+			powerupButton.addActionListener(al);
+			timedButton.addActionListener(al);
+			
+			//make it so that the dialog is displayed as it should be
+			dialog.setVisible(true);
+			dialog.pack();
+			dialog.setLocationRelativeTo(null);
+			//add that when on the textfield 'enter' is clicked or the dialogbox is closed to add to the highscore
+			
+	}
+	
+	public void startClassic(){
+		try {
+			SinglePlayerFrame frame = new SinglePlayerFrame();
+			frame.pack();
+			frame.setLocationRelativeTo(this.singlePlayerFrame);
+			this.singlePlayerFrame.dispose();
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			frame.getFindMultiplayerAction().setEnabled(true);
+		}
+		catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+	
+	public void startPowerup(){
+		try {
+			SinglePlayerFrame frame = new SinglePlayerFrame();
+			frame.pack();
+			frame.setLocationRelativeTo(this.singlePlayerFrame);
+			this.singlePlayerFrame.dispose();
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			frame.getFindMultiplayerAction().setEnabled(true);
+		}
+		catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+	
+	public void startDrunk(){
+		try {
+			SinglePlayerFrame frame = new SinglePlayerFrame();
+			frame.pack();
+			frame.setLocationRelativeTo(this.singlePlayerFrame);
+			this.singlePlayerFrame.dispose();
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			frame.getFindMultiplayerAction().setEnabled(true);
+		}
+		catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+	
+	public void startTimed(){
 		try {
 			SinglePlayerFrame frame = new SinglePlayerFrame();
 			frame.pack();
